cbf9219e4fdcb6d4c063a6b82255ee474e60a379
Awad <34748292+Awaddd@users.noreply.github.com>
2023-07-07 00:42:52 +0100

    Add provider scope to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:salah_app/core/templates/template.dart';
 import 'package:salah_app/core/utils/color_schemes.g.dart';
 import 'package:salah_app/features/home/home.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
